ef1ac7482e30b7d8cef1c67f505cc2148b5cb72f
Yug34 <vertilax1337@gmail.com>
2024-02-18 04:09:11 +0530

    :sparkles: feat(project): it works! :) added a join query

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
 import './App.css'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import alasql from "alasql";
 import {DATA_FILES} from "./constants.ts";
 
 const App = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
     useEffect(() => {
-        DATA_FILES.forEach(({fileName, tableName}) => {
+        Promise.all(DATA_FILES.map(({fileName, tableName}) => {
             fetch(`data/${fileName}`)
                 .then(response => response.text())
                 .then(csvData => {
                     alasql(`CREATE TABLE ${tableName}`);
                     alasql(`INSERT INTO ${tableName} SELECT * FROM CSV(?, {headers: true, separator:","});`, [csvData])
                 });
+        })).then(() => {
+            setIsLoaded(true)
         });
-
-        console.log(alasql.tables)
     }, []);
 
     return (
         <div>
-            Hello world
-            <button onClick={async () => {
+            {isLoaded ? (
+                <div>Loaded</div>
+            ) : (
+                <div>Not yet Loaded</div>
+            )}
+            <button onClick={() => {
                 console.log(alasql(`SELECT * FROM categories`))
             }}>
-                click
+                categories
+            </button>
+            <button onClick={() => {
+                console.log(alasql(`SELECT * FROM products WHERE `))
+            }}>
+                products
+            </button>
+            <button onClick={() => {
+                console.log(alasql(`SELECT t.territoryDescription
+                                    FROM territories t
+                                    JOIN employee_territories et ON t.territoryID = et.territoryID
+                                    WHERE et.employeeID = 2;
+                                    `))
+            }}>
+                join query
             </button>
         </div>
     )
