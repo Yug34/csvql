8c622bd5a0bfca049b8e59fd5f03ba03c04347ac
Yug34 <vertilax1337@gmail.com>
2024-02-20 20:08:15 +0530

    wip: csv upload presentation

--- src/components/CSVUpload.tsx
+++ src/components/CSVUpload.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {ChangeEvent} from "react";
+import {ChangeEvent, useState} from "react";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
@@ -8,10 +8,13 @@ import alasql from "alasql";
 import {useTablesStore} from "@/store/tablesStore.ts";
 import {roundNumber} from "@/lib/utils.ts";
 import {useAlasqlStore} from "@/store/alasqlStore.ts";
+import {Button} from "@/components/ui/button.tsx";
 
 export default function CSVUpload() {
-    const {addTable} = useTablesStore();
+    const {addTable, tables} = useTablesStore();
     const {setQuery, setData, setQueryExecutionTime} = useAlasqlStore();
+
+    const [csvData, setCSVData] = useState<null | string>(null);
 
     const getCSVDataFromFile = (event: ChangeEvent) => {
         const file = (event.target as HTMLInputElement).files![0];
@@ -19,20 +22,18 @@ export default function CSVUpload() {
 
         reader.onload = async (e) => {
             const contents = e.target!.result;
-            const tableName = "new_table";
-            addTable(tableName);
-
-            await alasql.promise(`CREATE TABLE ${tableName}`);
-            await alasql.promise(`INSERT INTO ${tableName} SELECT * FROM CSV(?, {headers: true, separator:","});`, [contents]);
-
-            let startTime = performance.now();
-            const query = `-- Enter SQL Query here:\nSELECT * FROM ${tableName}`;
-            setQuery(query);
-            setData(alasql(query));
-
-            let endTime = performance.now();
-            let timeElapsed = endTime - startTime;
-            setQueryExecutionTime(roundNumber(timeElapsed));
+            setCSVData(contents as string);
+            // const tableName = "new_table";
+            // addTable(tableName);
+            // await alasql.promise(`CREATE TABLE ${tableName}`);
+            // await alasql.promise(`INSERT INTO ${tableName} SELECT * FROM CSV(?, {headers: true, separator:","});`, [contents]);
+            // let startTime = performance.now();
+            // const query = `-- Enter SQL Query here:\nSELECT * FROM ${tableName}`;
+            // setQuery(query);
+            // setData(alasql(query));
+            // let endTime = performance.now();
+            // let timeElapsed = endTime - startTime;
+            // setQueryExecutionTime(roundNumber(timeElapsed));
         };
 
         reader.readAsText(file);
@@ -41,38 +42,56 @@ export default function CSVUpload() {
     return (
         <div className={"flex flex-col h-full justify-center items-center w-full"}>
             <Card className={"w-full border-none p-0"}>
-                <CardHeader>
-                    <CardTitle className="mb-3 flex justify-between items-center">
-                        <div>Import a CSV file</div>
-                    </CardTitle>
-                </CardHeader>
+                {csvData ? (
+                    <>
+                        <CardHeader>
+                            <CardTitle className="mb-3 flex justify-between items-center">
+                                <div>Name your new table</div>
+                            </CardTitle>
+                        </CardHeader>
 
-                <CardContent>
-                    <Label htmlFor="dropzone-file" className={"cursor-pointer"}>
-                        <Card
-                            className="flex p-4 items-center justify-center w-full brightness-[0.95] hover:brightness-[0.90] dark:brightness-125 dark:hover:brightness-150">
-                            <div className="text-center w-full">
-                                <div className="border p-2 rounded-md max-w-min mx-auto">
-                                    <UploadIcon/>
-                                </div>
+                        <CardContent>
+                            <Input type={"text"} placeholder={"Enter new table name"} name="table_name" onChange={(e) => {
+                                console.log(e.target.value);
+                            }}/>
+                            <div className={"whitespace-pre"}>{csvData}</div>
+                            <Button className={"w-full mt-6"} onClick={() => {}}>Create table</Button>
+                        </CardContent>
+                    </>
+                    ) : (
+                    <>
+                        <CardHeader>
+                            <CardTitle className="mb-3 flex justify-between items-center">
+                                <div>Import a CSV file</div>
+                            </CardTitle>
+                        </CardHeader>
 
-                                <p className="my-2 text-sm text-gray-500 dark:text-gray-400">
-                                <span className="font-semibold">
-                                  Click here to upload a CSV file
-                                </span>
-                                </p>
-                            </div>
-                        </Card>
-                    </Label>
+                        <CardContent>
+                            <Label htmlFor="dropzone-file" className={"cursor-pointer"}>
+                                <Card
+                                    className="flex p-4 items-center justify-center w-full brightness-[0.95] hover:brightness-[0.90] dark:brightness-125 dark:hover:brightness-150">
+                                    <div className="text-center w-full">
+                                        <div className="border p-2 rounded-md max-w-min mx-auto">
+                                            <UploadIcon/>
+                                        </div>
 
-                    <Input
-                        id="dropzone-file"
-                        accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-                        type="file"
-                        className="hidden"
-                        onChange={getCSVDataFromFile}
-                    />
-                </CardContent>
+                                        <p className="my-2 text-sm text-gray-500 dark:text-gray-400">
+                                            <span className="font-semibold">Click here to upload a CSV file</span>
+                                        </p>
+                                    </div>
+                                </Card>
+                            </Label>
+
+                            <Input
+                                id="dropzone-file"
+                                accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                                type="file"
+                                className="hidden"
+                                onChange={getCSVDataFromFile}
+                            />
+                        </CardContent>
+                    </>
+                    )}
             </Card>
         </div>
     );
