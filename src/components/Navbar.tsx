e7c20e9a45514455600001fbe61e7a2962424c46
Yug34 <yug.gjr@gmail.com>
2025-10-06 02:08:58 +0530

    add github to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,24 @@
-import {ModeToggle} from "@/components/ui/theme-toggler.tsx";
+import { ModeToggle } from "@/components/ui/theme-toggler.tsx";
+import { navigationMenuTriggerStyle } from "./ui/navigation-menu";
+import { Github } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
-    return (
-        <header className="flex items-center h-14 px-4 border-b">
-            <div className="mr-6 flex items-center gap-2">
-                Client-side SQL Editor
-            </div>
-            <nav className="flex-1 justify-end hidden md:flex">
-                <ModeToggle/>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="flex items-center h-14 px-4 border-b">
+      <div className="mr-6 flex items-center gap-2 font-bold">CSVQL</div>
+      <a
+        rel={"noreferrer"}
+        target={"_blank"}
+        href="https://github.com/Yug34/csvql"
+        className={cn(navigationMenuTriggerStyle(), "border-2 border-gray-500")}
+      >
+        <Github className={"mr-2"} />
+        Source Code
+      </a>
+      <nav className="flex-1 justify-end hidden md:flex">
+        <ModeToggle />
+      </nav>
+    </header>
+  );
 }
